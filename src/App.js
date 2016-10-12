af822c9ae245e4a861857ebe2c1bcaf497fc637b
bartosz.baczek <bartosz.baczek@gmail.com>
2016-10-12 18:34:56 +0200

    added bootstrap and removed Radek's photos

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>Welcome to 6hdz website</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          There will be website soon
         </p>
-        <img
-        style={{width: 200, height: 200}}
-        src={'https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/542487_397868403614289_100432499_n.jpg?oh=03c9a75b5d5aeebeed641914da7973fb&oe=5865B7D5'} alt="boohoo" className="img-responsive"/>
-        <img
-        style={{width: 200, height: 200}}
-        src={'https://scontent-mrs1-1.xx.fbcdn.net/v/t34.0-12/14527505_1174241999310255_1899457905_n.jpg?oh=d3495f4771c5a64b92f505a3059c6541&oe=57FD8CBA'} alt="boohoo" className="img-responsive"/>
       </div>
     );
   }
