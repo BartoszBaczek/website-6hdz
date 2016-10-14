523cb6883da6833db31054c2019c60e0b064d95e
bartosz.baczek <bartosz.baczek@gmail.com>
2016-10-14 21:59:35 +0200

    added proper positions to navbar

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { Nav, NavItem } from 'react-bootstrap';
+import { Navbar, NavItem, Nav } from 'react-bootstrap';
 
 class NavigationBar extends Component {
     render() {
         return (
-             <Nav bsStyle="pills">
-                 <NavItem>Lorem</NavItem>
-                 <NavItem>Ipsum</NavItem>
-                 <NavItem>Domanum</NavItem>
-            </Nav>
+             <Navbar inverse>
+                 <Navbar.Header>
+                     <Navbar.Brand>
+                        <a href="#">6HDŻ</a>
+                     </Navbar.Brand>
+                     <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">Aktualności</NavItem>
+                        <NavItem eventKey={2} href="#">O nas</NavItem>
+                        <NavItem eventKey={3} href="#">Media</NavItem>
+                        <NavItem eventKey={5} href="#">Lokalizacja</NavItem>
+                        <NavItem eventKey={6} href="#">Nasi partnerzy</NavItem>
+                        <NavItem eventKey={7} href="#">Kontakt</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
