e1d518e6ff2d6bc18963dedf9681248991d2361b
bartosz.baczek <bartosz.baczek@gmail.com>
2016-12-07 20:38:46 +0100

    back to previous component & it works now

--- src/ContactComponent/Contact.jsx
+++ src/ContactComponent/Contact.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import GoogleMap from 'google-map-react';
 import './Contact.css';
 
 class Contact extends Component {
     render() {
         return (
-                <h1>Narzygalem se do dupy</h1>
+            <div className="map">
+                <GoogleMap
+                    defaultCenter={{lat: 59.938043, lng: 30.337157}}
+                    defaultZoom={9}>
+                </GoogleMap>
+            </div>
         );
     }
 }
