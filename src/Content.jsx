3c22305c065c98383743a6e3b63b9a75d701a780
bartosz.baczek <bartosz.baczek@gmail.com>
2016-12-07 20:43:18 +0100

    fixed scrolling to Contact section

--- src/Content.jsx
+++ src/Content.jsx
@@ -27,7 +27,7 @@ class Content extends Component {
                 <Element name="section-6" className="element background-gray">
                     Nasi partnerzy
                 </Element>
-                <Element name="section-3" className="contact-page">
+                <Element name="section-7" className="contact-page">
                     <h1 className="contact-header">Kontakt</h1>
                     <Contact />
                 </Element>
