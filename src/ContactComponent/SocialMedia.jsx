21a73b0168ed067048888ead5fde73186c4db260
bartosz.baczek <bartosz.baczek@gmail.com>
2016-12-09 15:34:30 +0100

    SocialMedia component shows all four social media icons

--- src/ContactComponent/SocialMedia.jsx
+++ src/ContactComponent/SocialMedia.jsx
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import './SocialMedia.css';
 import { Image } from 'react-bootstrap';
 var socialMediaData = require("../../data/socialMedia.json");
+import { Col, Row } from 'react-bootstrap';
 
 class SocialMedia extends Component {
     render() {
         return (
-            <Image src={process.env.PUBLIC_URL + socialMediaData.socialMedia[0].logo} responsive/>
+            <Row className="margin-top-2">
+                <Row>
+                    <Col md={6}>
+                        <Image src={process.env.PUBLIC_URL + socialMediaData.socialMedia[0].logo} responsive/>
+                    </Col>
+                    <Col md={6}>
+                        <Image src={process.env.PUBLIC_URL + socialMediaData.socialMedia[1].logo} responsive/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                        <Image src={process.env.PUBLIC_URL + socialMediaData.socialMedia[2].logo} responsive/>
+                    </Col>
+                    <Col md={6}>
+                        <Image src={process.env.PUBLIC_URL + socialMediaData.socialMedia[3].logo} responsive/>
+                    </Col>
+                </Row>
+            </Row>
+
         );
     }
 }
