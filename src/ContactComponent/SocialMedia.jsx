c1c66a5068604a63f2e807145f058148b38c1525
bartosz.baczek <bartosz.baczek@gmail.com>
2016-12-11 15:25:08 +0100

    installed font-awesome

--- src/ContactComponent/SocialMedia.jsx
+++ src/ContactComponent/SocialMedia.jsx
@@ -8,6 +8,10 @@ class SocialMedia extends Component {
     render() {
         return (
             <Row className="margin-top-2">
+                <Col md={12}>
+                    <i className="fa fa-camera-retro fa-5x"></i> fa-5x
+                    <h3>6 Harcerska Drużyna Żeglarska im. kmdr. por. Zbigniewa Przybyszewskiego w Rybniku</h3>
+                </Col>
                 <Row>
                     <Col md={6}>
                         <Image src={process.env.PUBLIC_URL + socialMediaData.socialMedia[0].logo} responsive/>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
+
 
 ReactDOM.render(
   <App />,
