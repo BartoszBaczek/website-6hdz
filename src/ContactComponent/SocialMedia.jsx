15cc9f0f1f760901051fb576bec1507ac2f657ee
bartosz.baczek <bartosz.baczek@gmail.com>
2016-12-12 17:56:11 +0100

    removed social media data & added social media as font-awesome icons

--- src/ContactComponent/SocialMedia.jsx
+++ src/ContactComponent/SocialMedia.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './SocialMedia.css';
-import { Image } from 'react-bootstrap';
-var socialMediaData = require("../../data/socialMedia.json");
 import { Col, Row } from 'react-bootstrap';
 
 class SocialMedia extends Component {
@@ -9,9 +7,31 @@ class SocialMedia extends Component {
         return (
             <Row className="margin-top-2">
                 <Col md={11} mdOffset={1}>
+                    <h1><b>Skontaktuj się z nami</b></h1>
                     <p><i className="fa fa-map-marker" aria-hidden="true"></i>&nbsp; Stanica Wodna 6HDŻ, Rudzka, 44-200 Rybnik</p>
                     <p><i className="fa fa-phone" aria-hidden="true"></i>&nbsp; 604-293-288</p>
                     <p><i className="fa fa-envelope" aria-hidden="true"></i>&nbsp; 6hdz.zhprybnik@gmail.com</p>
+                        <h1><b>6HDŻ w internecie</b></h1>
+                        <p>
+                            <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                            &nbsp;
+                            <a target="_blank" href="http://wwww.facebook.com/6HDZ.Rybnik">6HDZ.Rybnik</a>
+                        </p>
+                        <p>
+                            <i className="fa fa-instagram" aria-hidden="true"></i>
+                            &nbsp;
+                            <a target="_blank" href="http://www.instagram.com/6hdz_rybnik/">6hdz_rybnik</a>
+                        </p>
+                        <p>
+                            <i className="fa fa-youtube-play" aria-hidden="true"></i>
+                            &nbsp;
+                            <a target="_blank" href="http://www.youtube.com/channel/UChEo0OpIE9_mJShfOFnNYNg">6. HARCERSKA DRUŻYNA ŻEGLARSKA</a>
+                        </p>
+                        <p>
+                            <i className="fa fa-snapchat-square" aria-hidden="true"></i>
+                            &nbsp;
+                            <a target="_blank" href="http://www.snapchat.com/add/wodniacyrybnik">@wodniacyrybnik</a>
+                        </p>
                 </Col>
             </Row>
         );
