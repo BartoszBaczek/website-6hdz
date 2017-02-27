b8f1445502017d3347919e1d5ea9b1b56a5ccb13
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-02-27 22:23:17 +0100

    google maps work now - i should propably hide API key though

--- src/ContactComponent/Contact.jsx
+++ src/ContactComponent/Contact.jsx
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import GoogleMap from 'google-map-react';
-import './Contact.css';
+import { Col, Row } from 'react-bootstrap';
+import { Gmaps, Marker, InfoWindow, Circle } from 'react-gmaps';
 import Message from './Message';
 import SocialMedia from './SocialMedia';
-import { Col, Row } from 'react-bootstrap';
+import './Contact.css';
+
+const coords = {
+    lat: 50.1259592,
+    lng: 18.5053484
+}
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                <Col className="map">
-                    <GoogleMap
-                        defaultCenter={{lat: 50.1259985, lng: 18.50548}}
-                        defaultZoom={16}
-                        bootstrapURLKeys={{key: "AIzaSyCxCOKEipbYo0RgQtVaPD_IkTuzfG7dovI"}}>
-                    </GoogleMap>
-                </Col>
+                <Gmaps
+                    height={'350px'}
+                    lat={coords.lat}
+                    lng={coords.lng}
+                    zoom={15}
+                    params={{v: '3.exp', key: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'}}>
+                    <Marker
+                        lat={coords.lat}
+                        lng={coords.lng}
+                     />
+                </Gmaps>
                 <Row>
                     <Col md={4}>
                         <SocialMedia />
