4d7635bcbb2b2b49871e224a54023137d8f2811e
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-03-01 16:48:47 +0100

    removed shitty selection effect on hover

--- src/AboutUsComponent/Portrait.jsx
+++ src/AboutUsComponent/Portrait.jsx
@@ -9,8 +9,8 @@ class Portrait extends Component {
                 <Image src={process.env.PUBLIC_URL + this.props.photo} responsive/>
                 <div className="overlay">
                     <h2>{this.props.scoutRank} {this.props.nameSurname}</h2>
-                    <p className="info">{this.props.yachtRank}</p>
-                    <p className="info">{this.props.role}</p>
+                    <p>{this.props.yachtRank}</p>
+                    <p>{this.props.role}</p>
                 </div>
             </div>
         );
