75088ad493f98d666b755fb2aaf415f9405ccb35
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-03-04 15:39:11 +0100

    removed redundant folders with placeholders

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const aboutUsRouter = require('./src/backend/routers/aboutUsRouter');
 
-
 let app = express();
 
 app.set('port', (process.env.PORT || 3001));
@@ -9,7 +8,6 @@ app.set('port', (process.env.PORT || 3001));
 app.use(express.static('public'));
 
 app.use('/api', aboutUsRouter);
-
 
 app.listen(app.get('port'), () => {
     console.log(`App listening on port ${app.get('port')}`);
