057055adcfc2a103a06a207d7ff331b99f87b76e
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-03-04 12:53:15 +0100

    merged App.jsx, Content.jsx and navbar.jsx into one componetn (App.jsx)

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
-import './App.css';
-import NavigationBar from './NavigationBar';
-import Content from './Content';
+import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import { Link, Events, animateScroll, Element } from 'react-scroll';
 
+import AboutUs from './pages/aboutUs/AboutUs';
+import Contact from './pages/contact/Contact';
+import Footer from './pages/footer/Footer';
+import Header from './pages/header/Header';
+
+import './App.css';
+import './NavigationBar.css';
+import './Content.css';
 
 
 class App extends Component {
+  componentDidMount() {
+    Events.scrollEvent.register('begin');
+    Events.scrollEvent.register('end');
+  }
+
+  scrollToTop() {
+    animateScroll.scrollToTop({smooth: true, duration: 500});
+  }
   render() {
     return (
         <div className="color-schema">
-            <NavigationBar />
-            <Content />
+          <Navbar inverse fixedTop={true}>
+                 <Navbar.Header>
+                    <img className="home-logo" alt="Brand" src="./../favicon.ico" onClick={this.scrollToTop}></img>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavItem><Link to="news" smooth={true} duration={500}>Aktualności</Link></NavItem>
+                        <NavItem><Link to="aboutUs" smooth={true} duration={500}>O nas</Link></NavItem>
+                        <NavItem><Link to="partners" smooth={true} duration={500}>Nasi partnerzy</Link></NavItem>
+                        <NavItem><Link to="contact" smooth={true} duration={500}>Kontakt</Link></NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className="content">
+                <Header />
+                <Element name="news" className="page-block">
+                    Aktualności
+                </Element>
+                <Element name="aboutUs" className="page-block">
+                    <h1 className="about-us-header">Nasz kadra</h1>
+                    <AboutUs />
+                </Element>
+                <Element name="partners" className="page-block">
+                    Nasi partnerzy
+                </Element>
+                <Element name="contact" className="page-block">
+                    <h1 className="contact-header">Kontakt</h1>
+                    <Contact />
+                </Element>
+                <Footer />
+            </div>
         </div>
     );
   }
