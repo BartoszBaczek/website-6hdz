6181cf4ae8b92c341312b4be69b021cfeecc4234
Bartosz Baczek <bartosz.baczek@gmail.com>
2017-03-11 18:19:27 +0100

    made app deployable on heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const aboutUsRouter = require('./src/backend/routers/aboutUsRouter');
-const partnersRouter = require('./src/backend/routers/partnersRouter');
+const aboutUsRouter = require('./server/backend/routers/aboutUsRouter');
+const partnersRouter = require('./server/backend/routers/partnersRouter');
 
 let app = express();
 
 app.set('port', (process.env.PORT || 3001));
 
-app.use(express.static('public'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+}
 
 app.use('/api', aboutUsRouter);
 app.use('/api', partnersRouter);
