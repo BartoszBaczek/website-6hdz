7923033585a2f96fcc61721eb7e5acc30eb7b652
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-04-25 16:41:58 +0200

    added router reference to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const aboutUsRouter = require('./server/backend/routers/aboutUsRouter');
 const partnersRouter = require('./server/backend/routers/partnersRouter');
+const newsRouter = require('./server/backend/routers/newsRouter');
 
 let app = express();
 
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use('/api', aboutUsRouter);
 app.use('/api', partnersRouter);
+app.use('/api', newsRouter);
 
 
 app.listen(app.get('port'), () => {
