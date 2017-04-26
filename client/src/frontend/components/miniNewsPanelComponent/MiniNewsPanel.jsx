018efa6c047d778f8b779c8c555c2753cf72f81e
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-04-26 19:54:58 +0200

    refactor

--- client/src/frontend/components/miniNewsPanelComponent/MiniNewsPanel.jsx
+++ client/src/frontend/components/miniNewsPanelComponent/MiniNewsPanel.jsx
@@ -10,7 +10,7 @@ class MiniNewsPanel extends Component {
         if (this.props.data) {
             for (let i = 0; i <= this.props.data.length - 1; i++) {
                 miniNews.push(
-                    <Col md={3} className="mini-news-placeholder">  
+                    <Col key={i} md={3} className="mini-news-placeholder">  
                         <MiniNews data={this.props.data[i]}/>
                     </Col>
                 )
